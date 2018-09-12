14f46c7cb2e14c8a2cdf46e69e10da35fc30c140
Tomochika Fujisawa <t.fujisawa05@gmail.com>
2018-09-12 13:35:18 +0900

    correct RAxML commands in shell scripts

--- run.raxml.rapid.sh
+++ run.raxml.rapid.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 for sq in "$@"
 do
-	raxml -m GTRGAMMA -T 2 -f d -p 12345 -s $sq -n ${sq//.fasta} -o  Agkistrodon_contortrix_1,Agkistrodon_contortrix_2,Agkistrodon_piscivorus_1,Agkistrodon_piscivorus_2
+	raxmlHPC -m GTRGAMMA -T 2 -f d -p 12345 -s $sq -n ${sq//.fasta} -o  Agkistrodon_contortrix_1,Agkistrodon_contortrix_2,Agkistrodon_piscivorus_1,Agkistrodon_piscivorus_2
 done
